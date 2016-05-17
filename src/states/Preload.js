947726c5e617823c580d1f9fbfc0a328abf3f5f7
Dennis Liaw <dennis.liaw@gmail.com>
2016-05-17 14:27:45 -0400

    adds background sprites and sun/moon sprites & effects

--- src/states/Preload.js
+++ src/states/Preload.js
@@ -5,7 +5,14 @@ class Preload extends Phaser.State {
 		//this.game.load.image('myImage', 'assets/my-image.png');
 		//this.game.load.audio('myAudio', 'assets/my-audio.wav');
 		//this.game.load.atlas('myAtlas', 'assets/my-atlas.png', 'assets/my-atlas.json');
-	}
+    this.game.load.image('mountains-back', 'assets/mountains-back.png');
+    this.game.load.image('mountains-mid1', 'assets/mountains-mid1.png');
+    this.game.load.image('mountains-mid2', 'assets/mountains-mid2.png');
+
+    this.game.load.image("sun", 'assets/sun.png');
+    this.game.load.image("moon", 'assets/moon.png');
+  }
+
 
 	create() {
 		//NOTE: Change to GameTitle if required
